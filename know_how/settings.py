55b25a084cd8d410abf3dcd20d1d4b5a28051a8e
SandyWyper <sandywyper@gmail.com>
2025-07-25 14:09:12 +0100

    Set DEBUG to False and update ALLOWED_HOSTS for production deployment

--- know_how/settings.py
+++ know_how/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+9f5*a*h4*453l@qz$&+n#@(6t%a^$b6@nnsv2k%hkq-dy8p0^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
 
 # Application definition
