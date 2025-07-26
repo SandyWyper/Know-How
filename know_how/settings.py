9a45be4a2e48d224fe7810034f9a0fe767e031ca
SandyWyper <sandywyper@gmail.com>
2025-07-26 15:48:25 +0100

    Refactor SECRET_KEY setting to use environment variable for enhanced security

--- know_how/settings.py
+++ know_how/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+9f5*a*h4*453l@qz$&+n#@(6t%a^$b6@nnsv2k%hkq-dy8p0^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
