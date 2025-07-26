1c2ffa9d6b63e26bcb4ef060ccabed1ea827d3d8
SandyWyper <sandywyper@gmail.com>
2025-07-26 15:29:38 +0100

    Update DEBUG setting to use environment variable for better configuration management

--- know_how/settings.py
+++ know_how/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+9f5*a*h4*453l@qz$&+n#@(6t%a^$b6@nnsv2k%hkq-dy8p0^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
