18457efad1d28fb1b397ec74b22b8097a44328d5
SandyWyper <sandywyper@gmail.com>
2025-08-06 16:33:41 +0100

    Add URL routing for listings app and implement ListingList view for displaying listings.

--- listings/views.py
+++ listings/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Listing
 
 # Create your views here.
-def index(request):
-    """returns a simple "Hello, World!" response"""
-    return HttpResponse("This was a " + request.method + " request, hello listings page!")
+class ListingList(generic.ListView):
+    model = Listing
+    template_name = "listings/listings.html"
+    # context_object_name = "object_list"
