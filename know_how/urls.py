1ddf1ce2b16ca81220f55c1c87087f1e489e5347
SandyWyper <sandywyper@gmail.com>
2025-08-06 16:33:32 +0100

    Update URL configuration to include listings app URLs and improve routing structure.

--- know_how/urls.py
+++ know_how/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path 
-from listings import views as listings_views
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('', listings_views.index),
+    path("", include("listings.urls"), name="listings-urls"),
     path('admin/', admin.site.urls),
 ]
