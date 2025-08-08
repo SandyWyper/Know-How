123eed73a2b563446a9d393d76a6c16f7b51c6b1
SandyWyper <sandywyper@gmail.com>
2025-08-08 14:49:24 +0100

    install and config tailwind css

--- know_how/settings.py
+++ know_how/settings.py
@@ -43,9 +43,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_summernote',
+    'tailwind',
+    'theme',
     'listings',
     'reviews'
 ]
+
+TAILWIND_APP_NAME = 'theme'
+
+if DEBUG:
+    # Add django_browser_reload only in DEBUG mode
+    INSTALLED_APPS += ['django_browser_reload']
+ 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -57,6 +66,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if DEBUG:
+    # Add django_browser_reload middleware only in DEBUG mode
+    MIDDLEWARE += [
+        "django_browser_reload.middleware.BrowserReloadMiddleware",
+    ]
 
 CSRF_TRUSTED_ORIGINS = ['https://*.herokuapp.com']
 
