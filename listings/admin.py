029bb3798143b977e905c59d9ee1c4d539402a8f
SandyWyper <sandywyper@gmail.com>
2025-08-08 09:57:09 +0100

    Enhance ListingAdmin by adding 'created_on' and 'updated_on' to list_display and updating list_filter to include 'created_on' for better data management.

--- listings/admin.py
+++ listings/admin.py
@@ -6,9 +6,9 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Listing)
 class ListingAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'slug', 'status')
+    list_display = ('title', 'slug', 'status', 'created_on', 'updated_on')
     search_fields = ['title']
-    list_filter = ('status',)
+    list_filter = ('status','created_on')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 # Register your models here.
