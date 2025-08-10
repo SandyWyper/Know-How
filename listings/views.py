ae8abb276323790fa867e14f44d6784e28d4673d
SandyWyper <sandywyper@gmail.com>
2025-08-10 12:38:41 +0100

    Add docstring to ListingList view: Clarify purpose of the view by indicating it displays a list of published listings.

--- listings/views.py
+++ listings/views.py
@@ -4,6 +4,7 @@ from .models import Listing
 
 # Create your views here.
 class ListingList(generic.ListView):
+    """Displays a list of published listings."""
     queryset = Listing.objects.filter(status=1).order_by("-created_on")
     template_name = "listings/index.html"
     paginate_by = 6
