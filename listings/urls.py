13acd966fd79fcedbffbbd7b7bee60818df13a1b
SandyWyper <sandywyper@gmail.com>
2025-08-10 13:15:07 +0100

    Add listing detail view and template

--- listings/urls.py
+++ listings/urls.py
@@ -1,6 +1,8 @@
+"""URL patterns for the listings app."""
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path("", views.ListingList.as_view(), name="home"),
+    path('<slug:slug>/', views.listing_detail, name='listing_detail'),
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Listing
 
@@ -9,3 +9,23 @@ class ListingList(generic.ListView):
     template_name = "listings/index.html"
     paginate_by = 6
     # context_object_name = "object_list"
+
+
+def listing_detail(request, slug):
+    """
+    Displays a single listing.
+
+    **Context**
+
+    ``listing``
+        An instance of :model:`listings.Listing`.
+
+    **Template**
+    
+    :template:`listings/listing_detail.html`
+    """
+
+    queryset = Listing.objects.filter(status=1)
+    listing = get_object_or_404(queryset, slug=slug)
+
+    return render(request, "listings/listing_detail.html", {"listing": listing})
