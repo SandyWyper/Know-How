6e12c43de6a7776da8d16abac9db75e035ca74d4
SandyWyper <sandywyper@gmail.com>
2025-08-11 13:12:12 +0100

    chore: remove comments

--- listings/models.py
+++ listings/models.py
@@ -16,10 +16,6 @@ class Listing(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    # event_date_start = models.DateTimeField(null=True, blank=True)
-    # event_date_end = models.DateTimeField(null=True, blank=True)
-    # event_spaces = models.IntegerField(default=1)
-    # event_spaces_available = models.IntegerField(default=1)
     status = models.IntegerField(choices=STATUS, default=0)
 
     def __str__(self):
